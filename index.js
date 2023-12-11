ff088fa40991311d8b02a0b3bfc6c57edafd89c8
philip0980 <127869236+philip0980@users.noreply.github.com>
2023-12-11 21:21:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+// Database
+const database = process.env.DATABASE;
+mongoose
+  .connect(database)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Creating middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({ origin: "http://127.0.0.1:3000", methods: "POST", credentials: true })
+);
+
+// Creating schema
+const newSchema = mongoose.model("User", {
+  name: { type: String },
+  score: { type: Number },
+});
+
+// Creating new User
+app.post("/", (req, res) => {
+  const { name, score } = req.body;
+  const User = new newSchema({ name, score });
+  User.save()
+    .then(() => {
+      console.log("Data collected");
+      res.send("Data collected"); // Send a response back
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal Server Error"); // Send an error response
+    });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}`);
+});
